2ac3ac1c4f407922469f29295f38f0bfa47937bf
Ashish Solankar <30354944+ashish-solankar@users.noreply.github.com>
2019-11-09 21:05:29 +0530

    First working draft.
    Just printing whatever message is provided in DB based on current date of system.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dry-day-calendar
 A free github pages site for study purpose, which will tell you whether it's a dry day today.
+Dry days are specific days when the sale of alcohol is not permitted. Most of the Indian states observe these days on major national festivals/occasions.
+Don't let Dry Day ruin your plans and must consider visiting <a href="ashish-solankar.github.io/dry-day-calendar">||is today a dry day?||</a> before making any plan.
---
+++ calendar.js
@@ -0,0 +1,31 @@
+//Calendar
+var url = 'https://api.myjson.com/bins/xwuk4';
+
+function showResult(){
+	let today = new Date();
+	let fullYear = today.getFullYear();
+	let month =  today.getMonth();
+	let day  = today.getDate();
+
+	let todayString = "" + fullYear + "" + month + "" + day ;
+
+	const resultDiv = document.getElementById("container");
+
+	$.getJSON(url, function(data) {
+
+		var i;
+
+		 for (i = 0; i < data.length; i++) {
+
+		 	if("\""+todayString+"\"" == JSON.stringify(data[i].date)){
+				resultDiv.innerHTML = JSON.stringify(data[i].description);
+				break;
+		 	}else{
+		 		resultDiv.innerHTML = "No you can enjoy your drink ...";
+		 	}
+
+		 }
+         
+    });
+
+}
